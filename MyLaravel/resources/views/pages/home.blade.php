73488105fc7541c277207b039aa32a6724555367
Diego Barbaresco <diego.bar@live.it>
2021-05-17 23:31:36 +0200

    Sezione centrale

--- MyLaravel/resources/views/pages/home.blade.php
+++ MyLaravel/resources/views/pages/home.blade.php
@@ -2,6 +2,10 @@
 @section('content')
     <div id="current-series" class="centering">
 
+        <div class="title">
+            <h2>CURRENT SERIES</h2>
+        </div>
+
         <div id="thumbs">
 
             @foreach ($data as $d)
@@ -18,5 +22,43 @@
             @endforeach
 
         </div>
+
+        <button>LOAD MORE</button>
+
+    </div>
+
+    <div id="cta" class="centering">
+        <ul>
+            <li>
+                <a href="">
+                    <img src="{{ asset('/storage/assets/buy-comics-digital-comics.png') }}" alt="">
+                    <span>DIGITAL COMICS</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('/storage/assets/buy-comics-merchandise.png') }}" alt="">
+                    <span>DC MERCHANDISE</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('/storage/assets/buy-comics-subscriptions.png') }}" alt="">
+                    <span>SUBSCRIPTION</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('/storage/assets/buy-comics-shop-locator.png') }}" alt="">
+                    <span>COMIC SHOP LOCATOR</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('/storage/assets/buy-dc-power-visa.svg') }}" alt="">
+                    <span>DC POWER VISA</span>
+                </a>
+            </li>
+        </ul>
     </div>
 @endsection
