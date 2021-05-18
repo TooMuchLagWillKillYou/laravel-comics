53dad027891db16d97a68acd9086277a2b726e46
Diego Barbaresco <diego.bar@live.it>
2021-05-18 15:12:08 +0200

    Footer

---
+++ MyLaravel/resources/views/components/footer.blade.php
@@ -0,0 +1,99 @@
+<footer>
+    
+    <div id="main-footer" class="centering">
+
+        <nav>
+            <div class="f-list">
+                <h2>DC COMICS</h2>
+                <ul>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Comics</a></li>
+                    <li><a href="">Movies</a></li>
+                    <li><a href="">Tv</a></li>
+                    <li><a href="">Games</a></li>
+                    <li><a href="">Videos</a></li>
+                    <li><a href="">News</a></li>
+                </ul>
+            </div>
+    
+            <div class="f-list">
+                <h2>SHOP</h2>
+                <ul>
+                    <li><a href="">Shop DC</a></li>
+                    <li><a href="">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+        
+            <div class="f-list">
+                <h2>DC</h2>
+                <ul>
+                    <li><a href="">Terms Of Use</a></li>
+                    <li><a href="">Privacy policy (new)</a></li>
+                    <li><a href="">Ad Choice</a></li>
+                    <li><a href="">Advertising</a></li>
+                    <li><a href="">Jobs</a></li>
+                    <li><a href="">Subscriptions</a></li>
+                    <li><a href="">Talent Workshops</a></li>
+                    <li><a href="">CPSC Certificates</a></li>
+                    <li><a href="">Ratings</a></li>
+                    <li><a href="">Shop Help</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </div>
+        
+            <div class="f-list">
+                <h2>SITES</h2>
+                <ul>
+                    <li><a href="">DC</a></li>
+                    <li><a href="">MAD Magazine</a></li>
+                    <li><a href="">DC Kids</a></li>
+                    <li><a href="">DC Universe</a></li>
+                    <li><a href="">DC Power Visa</a></li>
+                </ul>
+            </div>
+        </nav>
+    
+        <p>All Site Content TM and 2020 DC Entertainment, unless otherwise noted here, All rights reserved. Cookies Settings</p>
+    
+        <img src="{{ asset('/storage/assets/dc-logo-bg.png') }}" alt="" id="logo-bg">
+
+    </div>
+
+    <div id="bottom-bar" class="centering">
+        <button>SIGN-UP NOW!</button>
+
+        <div class="social">
+
+            <h2>FOLLOW US</h2>
+            
+            <ul>
+                <li>
+                    <a href="">
+                        <img src="{{ asset('/storage/assets/footer-facebook.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="{{ asset('/storage/assets/footer-twitter.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="{{ asset('/storage/assets/footer-youtube.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="{{ asset('/storage/assets/footer-pinterest.png') }}" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="{{ asset('/storage/assets/footer-periscope.png') }}" alt="">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</footer>
--- MyLaravel/resources/views/components/header.blade.php
+++ MyLaravel/resources/views/components/header.blade.php
@@ -1,28 +1,32 @@
-<div id="nav">
-    <div id="top-bar" class="centering">
-        <span>DC POWER VISA</span>
-        <span>ADDITIONAL DC SITES</span>
+<header>
+
+    <div id="nav">
+        <div id="top-bar" class="centering">
+            <span>DC POWER VISA</span>
+            <span>ADDITIONAL DC SITES</span>
+        </div>
+    
+        <nav class="centering">
+    
+            <img src="{{ asset('/storage/assets/dc-logo.png') }}" alt="">
+    
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+    
+            <input type="text" placeholder="Search">
+        </nav>
     </div>
-
-    <nav class="centering">
-
-        <img src="{{ asset('/storage/assets/dc-logo.png') }}" alt="">
-
-        <ul>
-            <li><a href="">CHARACTERS</a></li>
-            <li><a href="">COMICS</a></li>
-            <li><a href="">MOVIES</a></li>
-            <li><a href="">TV</a></li>
-            <li><a href="">GAMES</a></li>
-            <li><a href="">COLLECTIBLES</a></li>
-            <li><a href="">VIDEOS</a></li>
-            <li><a href="">FANS</a></li>
-            <li><a href="">NEWS</a></li>
-            <li><a href="">SHOP</a></li>
-        </ul>
-
-        <input type="text" placeholder="Search">
-    </nav>
-</div>
-
-<div id="jumbotron"></div>
+    
+    <div id="jumbotron"></div>
+    
+</header>
