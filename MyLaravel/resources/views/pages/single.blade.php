a87f309a6a4e6869f32818536b3ea8a9da9b5fea
Diego Barbaresco <diego.bar@live.it>
2021-05-20 09:14:44 +0200

    tables

--- MyLaravel/resources/views/pages/single.blade.php
+++ MyLaravel/resources/views/pages/single.blade.php
@@ -7,89 +7,95 @@
             <img src="{{ $item['thumb'] }}" alt="">
         </div>
     
-    
-        <div class="centering">
-    
-            <section>          
-                <div class="description">
-                    <h2>{{ $item['title'] }}</h2>
-    
-                    <div class="price">
-                        <span>U.S. Price</span>
-                        {{ $item['price'] }}
-                        <span>AVAILABLE</span>
-                    </div>
-    
-                    <p>{{ $item['description'] }}</p>
-                </div>
-                
-                <div>
-                    <img src="{{ asset('/storage/assets/adv.jpg') }}" alt="">
-                </div>
-            </section>
-    
-            <section>
-                <div class="talent">
+        <section class="centering">          
+            <div class="description">
+                <h2>{{ $item['title'] }}</h2>
 
-                    <h3>TALENT</h3>
-                    <table>
-                        <tr>
-                            <td>Art By</td>
-                            <td>
-                                @foreach ($item['artists'] as $artist)
+                <div class="price">
+                    <span>U.S. Price</span>
+                    {{ $item['price'] }}
+                    <span>AVAILABLE</span>
+                </div>
+
+                <p>{{ $item['description'] }}</p>
+            </div>
+            
+            <div class="adv">
+                <h3>ADVERTISEMENT</h3>
+                <img src="{{ asset('/storage/assets/adv.jpg') }}" alt="">
+            </div>
+        </section>
+
+        <section class="centering">
+            <div class="talent">
+
+                <table>
+                    <tr><th><h3>Talent</h3></th></tr>
+
+                    <tr>
+                        <td>Art By:</td>
+                        <td>
+                            @foreach ($item['artists'] as $artist)
+                                {{ $artist }},
+
+                                @if ($loop->last)
                                     {{ $artist }}
-                                @endforeach
-                            </td>
-                        </tr>
+                                @endif
+                            @endforeach
+                        </td>
+                    </tr>
 
-                        <tr>
-                            <td>Written By</td>
-                            <td>
-                                @foreach ($item['writers'] as $writer)
+                    <tr>
+                        <td>Written By:</td>
+                        <td>
+                            @foreach ($item['writers'] as $writer)
+                                {{ $writer }},
+
+                                @if ($loop->last)
                                     {{ $writer }}
-                                @endforeach
-                            </td>
-                        </tr>
-                    </table>
-                </div>
+                                @endif
+                            @endforeach
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            
+            <div class="specs">
                 
-                <div class="specs">
-                    
-                    <h3>SPECS</h3>
-                    <table>
-                        <tr>
-                            <td>
-                                Series:
-                            </td>
-                            <td>
-                                {{ $item['series'] }}
-                            </td>
-                        </tr>
-    
-                        <tr>
-                            <td>
-                                U.S. Price:
-                            </td>
-                            <td>
-                                {{ $item['price'] }}
-                            </td>
-                        </tr>
-    
-                        <tr>
-                            <td>
-                                On Sale Date:
-                            </td>
-                            <td>
-                                {{ $item['sale_date'] }}
-                            </td>
-                        </tr>
-                    </table>
-                </div>
+                <table>
+                    <tr><th><h3>Specs</h3></th></tr>
 
-            </section>
-    
-        </div>
+                    <tr>
+                        <td>
+                            Series:
+                        </td>
+                        <td>
+                            {{ $item['series'] }}
+                        </td>
+                    </tr>
 
+                    <tr>
+                        <td>
+                            U.S. Price:
+                        </td>
+                        <td>
+                            {{ $item['price'] }}
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            On Sale Date:
+                        </td>
+                        <td>
+                            {{ $item['sale_date'] }}
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+        </section>
+    
     </main>
 
 @endsection
